15181a7aee3baf9c50fd903bba750e41db0595e5
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-02-07 15:51:46 -0500

    Add Habitile project to projects list
    Signed-off-by: Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>

--- assets/projects/projects.ts
+++ assets/projects/projects.ts
@@ -1,9 +1,11 @@
+import habitile from './habitile'
 import nixOSConfiguration from './nixOSConfiguration'
 import p5Commander from './p5Commander'
 import particlePhysicsWorkshop from './particlePhysicsWorkshop'
 import thisWebsite from './thisWebsite'
 
 export default [
+  habitile,
   nixOSConfiguration,
   thisWebsite,
   p5Commander,
