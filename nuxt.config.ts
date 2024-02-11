9502d5f8bd43df9e6d532bc2a72c0127c11b71ea
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2024-02-11 09:56:12 -0500

    Update nuxt.config.ts
    Signed-off-by: Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/content'],
-  typescript: {
-    strict: true,
-  },
+  typescript: { strict: true },
   devtools: { enabled: true },
-  ignore: ['**/.direnv', '**/.devenv', '**/.git'],
+  ignore: ['**/.direnv', '**/.devenv'],
   content: {
-    watch: false,
-    documentDriven: true,
     highlight: {
       theme: {
         default: 'dark-plus',
-      },
-    },
-  },
-  vite: {
-    server: {
-      fs: {
-        allow: ['~/.vscode/extensions/vue.volar-*/dist/preview-bin/nuxi'],
       },
     },
   },
